39a0bac6959ff7a76db6c5d1aa8d92260822cda2
arthur <arthur.broudoux@gmail.com>
2024-09-16 14:57:34 +0200

    feat: Determine Poker Flush Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- levels/determinePokerFlush.js
+++ levels/determinePokerFlush.js
@@ -1,3 +1,43 @@
-function determinePokerFlush(hand) {}
+function determinePokerFlush(hand) {
+  if (hand.length !== 5) {
+    console.log("Please give 5 cards!");
+  }
 
-determinePokerFlush(["AS", "3S", "9S", "KS", "4S"]);
+  const index = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+  const suitHand = hand[0].slice(1, 2);
+
+  let valuesHand = [];
+
+  for (let i = 0; i < hand.length; i++) {
+    let val = hand[i].slice(0, 1);
+    let suit = hand[i].slice(1, 2);
+
+    if (suit !== suitHand) {
+      console.log("Different suits!");
+      return;
+    }
+
+    if (valuesHand.length === 0) {
+      valuesHand.push(val);
+      continue;
+    }
+
+    let indexPreviousVal = index.indexOf(valuesHand[i - 1]);
+    let indexVal = index.indexOf(val);
+    let modulo = (indexPreviousVal % 10) - 1;
+
+    if (indexVal < indexPreviousVal && indexVal < modulo) {
+      console.log("Not a flush!");
+      return;
+    }
+
+    valuesHand.push(val);
+  }
+
+  console.log("Flush!");
+}
+
+test1 = ["AS", "3S", "4S", "5S", "6S"];
+test2 = ["AS", "3S", "9S", "KS", "4S"];
+
+determinePokerFlush(test1);
